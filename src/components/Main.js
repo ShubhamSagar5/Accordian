902bd6ea61545de808b0b6d0bd571ba763648400
shubham <shubhamsagar628@gmail.com>
2024-01-02 15:13:12 +0530

    Multilple Selection

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,52 @@
 import dataInfo from "../utillis/data";
 import { useState } from "react";
-import Lists from "./Lists";
 
 const Main = () => {
   const [data, setData] = useState(dataInfo);
-  const [selected, setSelected] = useState(null);
 
-  return <div>
-    {
-        data && data.length > 0 ? <div>
-            {
-                data.map(item => <Lists item={item}/>)
-            }
-        </div> : null
+  const [enableMulti,setEnableMul] = useState(false)
+
+  const [selected,setSelected] = useState()
+
+  const [multipleSel,setMultipleSelc] = useState([])
+  const handleSingleSelc = (getId) => {
+    setSelected(getId)
+  }
+
+  const handleMulSelc = (getId) => {
+    const copyMul = [...multipleSel]
+    const findIndexOfCurrId = copyMul.indexOf(getId)
+
+    if(findIndexOfCurrId === -1){
+        copyMul.push(getId)
+    }else{
+        copyMul.splice(findIndexOfCurrId,1)
     }
-  </div>;
+
+    setMultipleSelc(copyMul)
+  }
+console.log(multipleSel)
+
+  return (
+    <div>
+    <button onClick={() => setEnableMul(!enableMulti)}>Enable Multiple Selection</button>
+      {data.map((item) => (
+        <div>
+        <div onClick={ enableMulti ? () => handleMulSelc(item.id) : ()=>handleSingleSelc(item.id)}>
+        <h2>{item.question}</h2>
+        <span>+</span>
+        </div>
+        <div>
+
+        </div>
+         {
+            enableMulti ? multipleSel.indexOf(item.id) !== -1 && <div>{item.answer}</div>:
+            selected === item.id ? (item.answer) : (<div></div>)
+            }
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Main;
